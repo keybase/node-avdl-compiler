c557459d9dd44323957d0647fd83144155213924
Frederick Akalin <akalin@keyba.se>
2018-09-25 16:11:38 -0700

    Get test to pass

--- test/files/sample_test.go
+++ test/files/sample_test.go
@@ -1,14 +1,25 @@
 package sample1
 
 import (
-	"context"
 	"testing"
 
+	"github.com/keybase/client/go/protocol/keybase1"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
 )
 
+type fakeSample struct{}
+
+func (fakeSample) GetBaz(ctx context.Context, arg GetBazArg) (keybase1.SigID, error) {
+	return keybase1.SigID(""), nil
+}
+
+func (fakeSample) Notifier(ctx context.Context, n int) error {
+	return nil
+}
+
 func TestCall(t *testing.T) {
-	protocol := SampleProtocol(nil)
+	protocol := SampleProtocol(fakeSample{})
 	getBazDesc := protocol.Methods["getBaz"]
 	args := getBazDesc.MakeArg()
 	_, err := getBazDesc.Handler(context.Background(), args)
