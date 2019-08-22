80f1a8240e8a757023b657ddd2150141fb2c97c4
Nathan Smith <nathan@keyba.se>
2019-08-22 16:11:13 -0400

    Clean up config

--- iced-jest-transformer.js
+++ iced-jest-transformer.js
@@ -1,9 +1,7 @@
 const Iced = require("iced-coffee-script");
 
 module.exports = {
-  process: src => {
-    return {
-      code: Iced.compile(src)
-    };
-  }
+  process: src => ({
+    code: Iced.compile(src)
+  })
 };
