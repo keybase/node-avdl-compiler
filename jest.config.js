525246b56edc098e565a1b27c5d3a4f7104965c8
Nathan Smith <nathan@keyba.se>
2019-08-26 10:09:46 -0400

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of file extensions your modules use
-  moduleFileExtensions: ["js", "iced"],
+  moduleFileExtensions: ["iced"],
 
   // The test environment that will be used for testing
   testEnvironment: "node",
