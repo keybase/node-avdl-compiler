91637e51cc981f1954435e018c699cb123c04f73
Nathan Smith <nathan@nathansmith.io>
2019-10-07 09:01:55 -0700

    Commit sample.py changes

--- test/files/sample.py
+++ test/files/sample.py
@@ -8,9 +8,9 @@ Input files:
 from dataclasses import dataclass, field
 from enum import Enum
 from typing import Dict, List, Optional, Union
-
-from dataclasses_json import config, dataclass_json
 from typing_extensions import Literal
+
+from dataclasses_json import config, DataClassJsonMixin
 
 import github.com.keybase.client.go.protocol.keybase1 as keybase1
 
@@ -92,43 +92,37 @@ class Trixie__FLAGGLE(DataClassJsonMixin):
 
 Trixie = Union[Trixie__NONE, Trixie__BOZO, Trixie__BIPPY, Trixie__AGGLE, Trixie__FLAGGLE]
 
-@dataclass_json
 @dataclass
-class Noozle__1:
+class Noozle__1(DataClassJsonMixin):
     version: Literal[1]
     1: Optional[str]
 
-@dataclass_json
 @dataclass
-class Noozle__2:
+class Noozle__2(DataClassJsonMixin):
     version: Literal[2]
     2: Optional[int]
 
 Noozle = Union[Noozle__1, Noozle__2]
 
-@dataclass_json
 @dataclass
-class Blurp__true:
+class Blurp__true(DataClassJsonMixin):
     b: Literal[true]
     true: Optional[str]
 
-@dataclass_json
 @dataclass
-class Blurp__false:
+class Blurp__false(DataClassJsonMixin):
     b: Literal[false]
     false: Optional[int]
 
 Blurp = Union[Blurp__true, Blurp__false]
 
-@dataclass_json
 @dataclass
-class Simple:
+class Simple(DataClassJsonMixin):
     s: Optional[Blurp] = field(default=None, metadata=config(field_name='s'))
 
 Hash = str
-@dataclass_json
 @dataclass
-class Cat:
+class Cat(DataClassJsonMixin):
     bird: Dict[str, Noozle] = field(metadata=config(field_name='bird'))
     bee: Dict[str, Noozle] = field(metadata=config(field_name='bee'))
     birds: Dict[str, Optional[List[Noozle]]] = field(metadata=config(field_name='birds'))
@@ -161,15 +155,13 @@ class TeamInviteCategoryStrings(Enum):
     PHONE = 'phone'
 
 
-@dataclass_json
 @dataclass
-class TeamInviteType__UNKNOWN:
+class TeamInviteType__UNKNOWN(DataClassJsonMixin):
     c: Literal[TeamInviteCategoryStrings.UNKNOWN]
     UNKNOWN: Optional[str]
 
-@dataclass_json
 @dataclass
-class TeamInviteType__SBS:
+class TeamInviteType__SBS(DataClassJsonMixin):
     c: Literal[TeamInviteCategoryStrings.SBS]
     SBS: Optional[int]
 
