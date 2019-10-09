50434191cf7d4f7c7c7391cca00ef20b07fba1f4
Maxwell Krohn <themax@gmail.com>
2019-10-09 15:05:09 -0400

    fix test

--- test/files/sample.py
+++ test/files/sample.py
@@ -1,6 +1,6 @@
 """sample.1
 
-Auto-generated to Python types by avdl-compiler v1.4.4 (https://github.com/keybase/node-avdl-compiler)
+Auto-generated to Python types by avdl-compiler v1.4.5 (https://github.com/keybase/node-avdl-compiler)
 Input files:
  - avdl/sample.avdl
 """
