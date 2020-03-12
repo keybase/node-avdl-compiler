abb6a88f40683bbfd9b8acf049dede9b78bfa81c
Maxwell Krohn <themax@gmail.com>
2020-03-12 09:06:57 -0400

    update jest

--- test/files/sample.py
+++ test/files/sample.py
@@ -1,6 +1,6 @@
 """sample.1
 
-Auto-generated to Python types by avdl-compiler v1.4.6 (https://github.com/keybase/node-avdl-compiler)
+Auto-generated to Python types by avdl-compiler v1.4.8 (https://github.com/keybase/node-avdl-compiler)
 Input files:
  - avdl/sample.avdl
 """
